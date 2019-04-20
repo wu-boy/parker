f4afbdb9dceeb8f7ee1f6c88128ab35c6ca286c2
wu-boy <wu.work@qq.com>
2019-04-20 19:36:19 +0800

    增加函数式编程demo

--- parker-common/src/main/java/com/wu/parker/common/demo/util/StreamMain.java
+++ parker-common/src/main/java/com/wu/parker/common/demo/util/StreamMain.java
@@ -1,12 +1,15 @@
 package com.wu.parker.common.demo.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * 函数式编程例子
  * @author wusq
- * @date 2019/4/19
+ * @date 2019/4/20
  */
 public class StreamMain {
 
@@ -40,17 +43,67 @@ public class StreamMain {
     }
 
     /**
+     * 过滤年龄小于20的数据
+     */
+    static void filter(){
+        userDemoList = userDemoList.stream().filter(o -> o.getAge() < 20).collect(Collectors.toList());
+    }
+
+    /**
+     * 获取id的List
+     */
+    static void getIdList(){
+        List<String> list = userDemoList.stream().map(UserDemo::getId).collect(Collectors.toList());
+        list.forEach(o -> System.out.println(o));
+    }
+
+    /**
      * 拼接ID
      */
     static void joinId(){
+
         String ids = userDemoList.stream().map(UserDemo::getId).collect(Collectors.joining(","));
         System.out.println(ids); // 输出a,b,c
+
+        // 加上前后缀
+        ids = userDemoList.stream().map(UserDemo::getId).collect(
+                Collectors.joining(",", "prifix-", "-suffix"));
+        System.out.println(ids); // 输出prifix-a,b,c-suffix
+
+    }
+
+    /**
+     * 年龄排序
+     */
+    static void sort(){
+        Collections.sort(userDemoList, Comparator.comparing(UserDemo::getAge));
+    }
+
+    /**
+     * 年龄求和
+     */
+    static void sum(){
+        int amout = userDemoList.stream().map(UserDemo::getAge).reduce(Integer::sum).get();
+        System.out.println(amout);
+    }
+
+    /**
+     * 打印
+     */
+    static void print(){
+        userDemoList.forEach(o -> System.out.println(o.getId() + o.getName() + o.getAge()));
     }
 
     public static void main(String[] args) {
 
         init();
 
-        joinId();
+        filter();
+        // joinId();
+        // sort();
+        // sum();
+        // getIdList();
+
+        print();
     }
 }
